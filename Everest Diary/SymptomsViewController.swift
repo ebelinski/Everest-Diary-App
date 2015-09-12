475ee39d9c4f76f2a9d51f9c1523c0a95f5e81fe
Eugene Belinski <eugeniub@gmail.com>
2015-09-12 17:28:44 -0500

    added keyboard notifications

--- Everest Diary/SymptomsViewController.swift
+++ Everest Diary/SymptomsViewController.swift
@@ -10,10 +10,39 @@ import UIKit
 
 class SymptomsViewController: UIViewController {
 
+    @IBOutlet var textFieldSymptom1: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        var tap: UITapGestureRecognizer = UITapGestureRecognizer(
+            target: self, action: "resignAll")
+        view.addGestureRecognizer(tap)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self,
+            selector: Selector("keyboardWasShown:"),
+            name: UIKeyboardWillShowNotification,
+            object: nil)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self,
+            selector: Selector("keyboardWillBeHidden:"),
+            name: UIKeyboardWillHideNotification,
+            object: nil)
+    }
+    
+    func keyboardWasShown(aNotification: NSNotification) {
+        let info : NSDictionary = aNotification.userInfo!
+        let kbSize = info.objectForKey(UIKeyboardFrameBeginUserInfoKey)?
+            .CGRectValue().size
+        let height = kbSize!.height
+    }
+    
+    func keyboardWillBeHidden(aNotification: NSNotification) {
+        
+    }
+    
+    func resignAll() {
+        self.textFieldSymptom1.resignFirstResponder()
     }
 
     @IBAction func symptomEditingDidBegin(sender: AnyObject) {
