ea93261dc223de82ebbcee322f07ef710fd1885a
Eugene Belinski <eugeniub@gmail.com>
2015-09-13 10:41:07 -0500

    added media buttons, IBOutlets, and IBAction

--- Everest Diary/SymptomsMediaViewController.swift
+++ Everest Diary/SymptomsMediaViewController.swift
@@ -10,10 +10,24 @@ import UIKit
 
 class SymptomsMediaViewController: UIViewController {
 
+    @IBOutlet var imageView1: UIImageView!
+    @IBOutlet var imageView2: UIImageView!
+    @IBOutlet var imageView3: UIImageView!
+    @IBOutlet var imageView4: UIImageView!
+    
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    @IBOutlet var button4: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func didPressMediaButton(sender: AnyObject) {
+        
     }
     
     @IBAction func didPressDoneButton(sender: AnyObject) {
