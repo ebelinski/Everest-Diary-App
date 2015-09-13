508130310de71e96c3b6a4940c4ef172f9d7523e
Eugene Belinski <eugeniub@gmail.com>
2015-09-13 11:25:57 -0500

    added code to show either camera or photo library for menu

--- Everest Diary/SymptomsMediaViewController.swift
+++ Everest Diary/SymptomsMediaViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SymptomsMediaViewController: UIViewController {
+class SymptomsMediaViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     @IBOutlet var imageView1: UIImageView!
     @IBOutlet var imageView2: UIImageView!
@@ -29,19 +29,44 @@ class SymptomsMediaViewController: UIViewController {
     @IBAction func didPressMediaButton(sender: AnyObject) {
         let menu = UIAlertController(title: "Add Media", message: nil, preferredStyle: .ActionSheet)
         menu.addAction(UIAlertAction(title: "Take a picture", style: UIAlertActionStyle.Default, handler: {
-            (action:UIAlertAction!) -> Void in
-            println("clicked")
+            (action: UIAlertAction!) -> Void in
+            self.takePicture()
         }))
         menu.addAction(UIAlertAction(title: "Choose existing picture", style: UIAlertActionStyle.Default, handler: {
-            (action:UIAlertAction!) -> Void in
-            println("clicked")
+            (action: UIAlertAction!) -> Void in
+            self.chooseExistingPicture()
         }))
         menu.addAction(UIAlertAction(title: "Create a recording", style: UIAlertActionStyle.Default, handler: {
-            (action:UIAlertAction!) -> Void in
-            println("clicked")
+            (action: UIAlertAction!) -> Void in
+            println("recording...?")
         }))
         menu.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil))
         self.presentViewController(menu, animated: true, completion: nil)
+    }
+    
+    func takePicture() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        
+        if (UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)) {
+            imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
+            imagePicker.cameraCaptureMode = UIImagePickerControllerCameraCaptureMode.Photo
+        } else {
+            imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        }
+        
+        self.presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    func chooseExistingPicture() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        self.presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        println("image chosen")
     }
     
     @IBAction func didPressDoneButton(sender: AnyObject) {
