80d19b538c2f9fd4783d21fa67f9c9d94683afd6
Eugene Belinski <eugeniub@gmail.com>
2015-09-12 22:28:20 -0500

    added medications taken question page

---
+++ Everest Diary/MedicationsTakenViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MedicationsTakenViewController.swift
+//  Everest Diary
+//
+//  Created by HHWS on 12/9/15.
+//  Copyright (c) 2015 Everest Team. All rights reserved.
+//
+
+import UIKit
+
+class MedicationsTakenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
